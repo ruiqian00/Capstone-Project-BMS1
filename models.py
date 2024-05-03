13593192f18c6f9bdb5a2b254e0010f5f66e8857
ruiqian00 <61444816+ruiqian00@users.noreply.github.com>
2024-05-02 21:45:04 -0400

    Update models.py
    Added densenet and Inception V3

--- models.py
+++ models.py
@@ -21,6 +21,22 @@ def create_model(model_name, num_classes, use_pretrained=True):
         model.classifier[6] = nn.Linear(num_ftrs, num_classes)
     elif model_name == 'swin_transformer':
         model = timm.create_model('swin_tiny_patch4_window7_224', pretrained=use_pretrained, num_classes=num_classes)
+    elif model_name == 'densenet121':
+        model = models.densenet121(pretrained=use_pretrained)
+        num_ftrs = model.classifier.in_features
+        model.classifier = nn.Linear(num_ftrs, num_classes)
+    elif model_name == 'densenet201':
+        model = models.densenet201(pretrained=use_pretrained)
+        num_ftrs = model.classifier.in_features
+        model.classifier = nn.Linear(num_ftrs, num_classes)
+    elif model_name == 'inception_v3':
+        model = models.inception_v3(pretrained=use_pretrained)
+        # handle auxiliary output
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, num_classes)
+        if model.AuxLogits is not None:
+            num_aux_ftrs = model.AuxLogits.fc.in_features
+            model.AuxLogits.fc = nn.Linear(num_aux_ftrs, num_classes)
     else:
         raise ValueError('Invalid model name')
     return model
